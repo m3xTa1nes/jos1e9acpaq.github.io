a91f0dc729b77ffb18e2c94c7fbcbf684f77ea2f
Debugger <Debugger@LAPTOP-GS90BMBR>
2024-12-16 01:01:46 +0300

    commit

--- bot.js
+++ bot.js
@@ -6,19 +6,4 @@ const WEB_APP_URL = 'https://m3xta1nes.github.io/';
 // Создаем экземпляр бота
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-bot.onText(/\/start/, async (msg) => {
-    await bot.sendMessage(msg.chat.id, '', {
-        reply_markup: {
-          keyboard: [
-            [
-              {
-                text: 'Открыть мини-приложение', 
-                web_app: { url: WEB_APP_URL }
-              }
-            ]
-          ],
-          resize_keyboard: true,
-          one_time_keyboard: true
-        }
-      });
-});
+bot.onText(/\/start/, async (msg) => {});
