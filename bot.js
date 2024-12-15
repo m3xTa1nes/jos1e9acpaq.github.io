7b3869467222ec40943db1777e0574b9ecb9883a
Debugger <Debugger@LAPTOP-GS90BMBR>
2024-12-16 00:43:12 +0300

    commit

--- bot.js
+++ bot.js
@@ -1,9 +1,24 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const BOT_TOKEN = '7983725176:AAGkGy0KbTxgph-JBGm5SOaKSzaeMP2ERLQ';
+const WEB_APP_URL = 'https://m3xta1nes.github.io/';
 
 // Создаем экземпляр бота
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-bot.onText(/\/start/, (msg) => {});
-
+bot.onText(/\/start/, async (msg) => {
+    await bot.sendMessage(msg.chat.id, '', {
+        reply_markup: {
+          keyboard: [
+            [
+              {
+                text: 'Открыть мини-приложение', 
+                web_app: { url: WEB_APP_URL }
+              }
+            ]
+          ],
+          resize_keyboard: true,
+          one_time_keyboard: true
+        }
+      });
+});
